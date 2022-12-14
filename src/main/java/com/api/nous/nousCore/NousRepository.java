e0577d4240f556d7271b4bfca5ff745d507034dd
Chris <thegtaguy08@outlook.com>
2022-12-14 18:12:27 -0500

    Repo for MYSQL created!

---
+++ src/main/java/com/api/nous/nousCore/NousRepository.java
@@ -0,0 +1,8 @@
+package com.api.nous.nousCore;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NousRepository extends JpaRepository<Nous, Integer> {
+
+    //This is the main repo for nous;
+}
